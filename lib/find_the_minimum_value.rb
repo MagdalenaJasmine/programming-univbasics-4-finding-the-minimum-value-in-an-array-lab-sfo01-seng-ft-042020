f52bef4fb1093256b16dbbd11cc7008e67c5ce33
Magdalena Cooper <magdalenacooper92@gmail.com>
2020-03-31 13:21:23 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -3,5 +3,9 @@ def find_min_value(array)
   max_value=100
   while count<array.length do
      if min_value<array[count]
-      max_value=array[count]
+      min_value=array[count]
+    end
+    count-=1 
+  end
+  min_value 
 end
